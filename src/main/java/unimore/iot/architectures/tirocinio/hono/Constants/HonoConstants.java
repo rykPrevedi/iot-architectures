681dde19a44077e0b8a36996f37c5b6699a2a24e
rykPrevedi <261903@studenti.unimore.it>
2023-02-25 19:06:01 +0100

    Hono re-installation changing ports and add Hono Mqtt protocol adapter ports

--- src/main/java/unimore/iot/architectures/tirocinio/hono/Constants/HonoConstants.java
+++ src/main/java/unimore/iot/architectures/tirocinio/hono/Constants/HonoConstants.java
@@ -11,13 +11,47 @@ public class HonoConstants {
     // (k8s-node-1) IP address of Kubernetes node where HONO is running
     public static final String HONO_HOST = "192.168.181.17";
 
+
+    /**
+     * SERVICE-DEVICE-REGISTRY
+     *
+     * Device registry port where applications can register the device and tenant and set the device password.
+     * Through the Device Registry Management API
+     */
     // (eclipse-hono-service-device-registry-ext)
-    // external Port where the endpoint can be reached by the HTTP protocol communication
-    public static final int HONO_HTTP_DEVICE_REGISTRY_PORT = 31735;
+    // external Port where the endpoint can be reached via the HTTP protocol communication
+    public static final int HONO_HTTP_DEVICE_REGISTRY_PORT = 30873;
 
     // (eclipse-hono-service-device-registry-ext)
-    // external Port where the endpoint can be reached by the HTTPS protocol communication
-    public static final int HONO_HTTPS_DEVICE_REGISTRY_PORT = 30659;
+    // external Port where the endpoint can be reached via the HTTPS protocol communication
+    public static final int HONO_HTTPS_DEVICE_REGISTRY_PORT = 31005;
+
+    /**
+     * AMQP-MESSAGING-NETWORK-ROUTER
+     *
+     * Port of the AMQP network where consumers can receive data (in the standard setup this is the port
+     * of the qdrouter).
+     */
+    // (eclipse-hono-dispatch-router-ext)
+    // external Port where the endpoint can be reached via the AMQP protocol communication
+    public static final int HONO_AMQP_CONSUMER_PORT = 32124;
+
+    // (eclipse-hono-dispatch-router-ext)
+    // external Port where the endpoint can be reached via the AMQPS protocol communication
+    public static final int HONO_AMQPS_CONSUMER_PORT = 31945;
+
+    /**
+     * MQTT-HONO-PROTOCOL-ADAPTER
+     *
+     * Port of the MQTT protocol adapter where the device can Publish messages or Subscribe to a specific topic
+     */
+    // (eclipse-hono-adapter-mqtt)
+    // external Port where the endpoint can be reached via the MQTT protocol communication
+    public static final int HONO_MQTT_ADAPTER_PORT = 31724;
+
+    // (eclipse-hono-adapter-mqtt)
+    // external Port where the endpoint can be reached via the MQTT protocol communication
+    public static final int HONO_SECURE_MQTT_ADAPTER_PORT = 30786;
 
     /**
      * Hono is designed to structure
@@ -26,7 +60,7 @@ public class HonoConstants {
      * for enabling a scalable distributed architecture
      * to handle independent subsets as if each subset had its own installation
      */
-    public static final String MY_TENANT_ID = "myTenant";
+    public static final String MY_TENANT_ID = "mytenant";
 
     private HonoConstants() { // prevent instantiation
     }
