c952a6ebc56c933dca4466b1cafb64dd87c52c60
rykPrevedi <261903@studenti.unimore.it>
2023-02-26 09:28:56 +0100

    Create class engine for temperature sensor values production

---
+++ src/main/java/unimore/iot/architectures/tirocinio/hono/devices/mqtt/model/EngineTemperatureSensor.java
@@ -0,0 +1,39 @@
+package unimore.iot.architectures.tirocinio.hono.devices.mqtt.model;
+
+import java.util.Random;
+
+/**
+ * @author Riccardo Prevedi
+ * @created 25/02/2023 - 21:19
+ * @project architectures-iot
+ */
+
+public class EngineTemperatureSensor {
+
+    private Random rnd;
+
+    private double temperatureValue;
+
+    public EngineTemperatureSensor() {
+        this.rnd = new Random(System.currentTimeMillis());
+        this.temperatureValue = 0.0;
+    }
+
+    private void generateEngineTemperature() {
+        temperatureValue =  80 + rnd.nextDouble() * 20.0;
+    }
+
+    public double getTemperatureValue() {
+        generateEngineTemperature();
+        return temperatureValue;
+    }
+
+    public void setTemperatureValue(double temperatureValue) {
+        this.temperatureValue = temperatureValue;
+    }
+
+    @Override
+    public String toString() {
+        return "EngineTemperatureSensor [temperatureValue=" + temperatureValue + "]";
+    }
+}
