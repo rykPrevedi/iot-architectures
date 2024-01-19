19cc883e967454a08d921fc56a0eb3f6057548e7
rykPrevedi <261903@studenti.unimore.it>
2024-01-19 16:24:29 +0100

    Add method publishHttpData

--- src/main/java/unimore/iot/architectures/tirocinio/hono/devices/http/HttpTelemetry.java
+++ src/main/java/unimore/iot/architectures/tirocinio/hono/devices/http/HttpTelemetry.java
@@ -1,47 +1,74 @@
 package unimore.iot.architectures.tirocinio.hono.devices.http;
 
+import com.google.gson.Gson;
 import kong.unirest.Unirest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import unimore.iot.architectures.tirocinio.hono.model.MessageDescriptor;
 
 import static unimore.iot.architectures.tirocinio.hono.constants.HonoConstants.*;
 
 /**
- * This class is a demo class
- * which allows to do a POST request with a  body {"temp": 5}
+ * Demo class that POST a request with a JSON body (sensor temperature value = 5)
  * and the message will be sent with the default QoS "at most once" (0)
- * *
- * *
+ * <p>
  * If the request is correct, the response will contain a message like:
  * HTTP/1.1 202 Accepted
  * content-length: 0
- * *
+ * <p>
  * else, if the request failed the response body will contain the error details.
  */
 
 public class HttpTelemetry {
     private static final Logger LOG = LoggerFactory.getLogger(HttpTelemetry.class);
-    private static final String URI = "/telemetry";
+    private static final String URI_PATH = "/telemetry";
+    private static final String USERNAME = httpDeviceAuthId + "@" + MY_TENANT_ID;
+    private static final String BASE_URL = String.format("http://%s:%d",
+            HONO_HOST,
+            HONO_HTTP_ADAPTER_PORT);
+    private static final Integer MESSAGE_COUNT = 10;
 
-    public static void main(String[] args) {
-        String message = "{\"temp\": 5}";
-        String username = httpDeviceAuthId + "@" + MY_TENANT_ID;
+    public HttpTelemetry() {
+    }
 
+    public static void main(String[] args) throws InterruptedException {
+        double sensorValue = 5;
+        String jsonString = buildJsonMessage(sensorValue);
+        HttpTelemetry httpTelemetry = new HttpTelemetry();
+
+        for (int i = 0; i < MESSAGE_COUNT; i++) {
+            httpTelemetry.publishHttpData(jsonString);
+            Thread.sleep(5000);
+        }
+    }
+
+    private void publishHttpData(String b) {
         Unirest
-                .post("http://" + HONO_HOST + ":" + HONO_HTTP_ADAPTER_PORT + URI)
-                .basicAuth(username, devicePassword)
+                .post(BASE_URL + URI_PATH)
+                .basicAuth(USERNAME, devicePassword)
                 .header("content-type", "application/json")
                 .header("qos-level", "0")
-                .body(message)
+                .body(b)
                 .asString()
-                .ifSuccess(res -> LOG.info("\nHTTP/1.1 {} {}\n{}",
-                        res.getStatus(),
-                        res.getStatusText(),
-                        res.getHeaders()))
-                .ifFailure(res -> { LOG.error("Oh No, Status: {} {}",
-                            res.getStatus(),
-                            res.getStatusText());
-                    LOG.error(res.getBody());
+                .ifSuccess(r -> LOG.info("\nHTTP/1.1 {} {}",
+                        r.getStatus(),
+                        r.getStatusText()))
+                .ifFailure(e -> {
+                    LOG.error("Oh No, Status: {} {}",
+                            e.getStatus(),
+                            e.getStatusText());
+                    LOG.error(e.getBody());
                 });
+    }
+
+    private static String buildJsonMessage(double sensorValue) {
+        try {
+            Gson gson = new Gson();
+            MessageDescriptor messageDescriptor = new MessageDescriptor(MessageDescriptor.HTTP_TEMPERATURE_SENSOR, sensorValue);
+            return gson.toJson(messageDescriptor);
+        } catch (Exception e) {
+            LOG.error("Error creating json payload ! Message: {}", e.getLocalizedMessage());
+            return null;
+        }
     }
 }
