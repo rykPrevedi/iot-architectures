71b7b15392a065c0cfc09de17cde5aa97744700b
rykPrevedi <261903@studenti.unimore.it>
2024-01-19 17:24:53 +0100

    MQTT event publisher

---
+++ src/main/java/unimore/iot/architectures/tirocinio/hono/devices/mqtt/MqttEPublisher.java
@@ -0,0 +1,79 @@
+package unimore.iot.architectures.tirocinio.hono.devices.mqtt;
+
+import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.UUID;
+
+import static unimore.iot.architectures.tirocinio.hono.constants.HonoConstants.*;
+
+/**
+ * Demo class that publish events on the "e" topic
+ * {} json string that will be sent
+ */
+
+public class MqttEPublisher {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MqttEPublisher.class);
+    private static final String TOPIC = "e";
+    private static final String METADATA = "/?content-type=application%2Fjson";
+    private static final String MQTT_BASE_URL = String.format("tcp://%s:%d",
+            HONO_HOST,
+            HONO_MQTT_ADAPTER_PORT);
+    private static final int QOS = 1;
+    private static IMqttClient client;
+    private static MqttConnectOptions options;
+
+    public MqttEPublisher() {
+        options = new MqttConnectOptions();
+        options.setUserName(mqttDeviceAuthId + "@" + MY_TENANT_ID);
+        options.setPassword(devicePassword.toCharArray());
+        options.setAutomaticReconnect(true);
+        options.setConnectionTimeout(10);
+    }
+
+    public static void main(String[] args) {
+        LOG.info("Client MQTT started ... ");
+        final String payloadString = "{\"alarm\": 1}";
+        try {
+            String clientId = UUID.randomUUID().toString();
+            MqttEPublisher producer = new MqttEPublisher();
+            client = new MqttClient(MQTT_BASE_URL, clientId, new MemoryPersistence());
+            producer.connect(options, clientId);
+            publishData(payloadString);
+            disconnect(clientId);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void publishData(String msgString) throws MqttException {
+        LOG.info("Publishing to Topic: {} Data: {}", TOPIC, msgString);
+        if (msgString != null) {
+            MqttMessage message = new MqttMessage(msgString.getBytes());
+            message.setQos(QOS);
+            message.setRetained(false);
+            client.publish(TOPIC + METADATA, message);
+            LOG.debug("Data Correctly Published !");
+        } else {
+            LOG.error("Error: Topic or Msg = Null or MQTT Client is not Connected !");
+        }
+    }
+
+    private void connect(MqttConnectOptions options, String clientId) throws MqttException {
+        IMqttToken iMqttToken = client.connectWithResult(options);
+        if (client.isConnected()) {
+            LOG.info("Connected to the HONO Mqtt Adapter ! ClientID: [{}]", clientId);
+            LOG.info("Context : {}", iMqttToken.getUserContext());
+        } else
+            LOG.error("connection could not be established");
+    }
+
+    private static void disconnect(String clientId) throws MqttException {
+        client.disconnect();
+        client.close();
+        LOG.info("[{}] Disconnects", clientId);
+    }
+}
